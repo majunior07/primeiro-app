8764c0dc5ae5d4333e704aa34f75773d85429e1a
Mauricio Augusto Junior | Hospital Unimed Poços de Caldas <email@email.com>
2023-03-03 19:00:44 -0300

    Segundo commit 03/03/23

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { useState } from 'react';
+
+import Nome from './components/Nome';
 
 function App() {
-  return (
+  const [aluno, setAluno] = useState('Sujeito Programador')
+
+  function handleChangeName(){
+    //alert('TESTE')
+    setAluno('Matheus');
+  }
+
+  return(
     <div>
-      <h1>Bem vindo ao meu projeto</h1>
+      <h1>Componente App</h1>
+      <br />
+      <h2>Olá: {aluno}</h2>
+      <button onClick={ handleChangeName }>
+        Mudar Nome
+      </button>
+    
+    
     </div>
   );
 }
 
 export default App;
-
-
 
