2416de9d24c046c722c64814f168a1348dd532f3
Mauricio Augusto Junior | Hospital Unimed Poços de Caldas <email@email.com>
2023-03-06 19:49:05 -0300

    Segundo commit 06/03/23

--- src/App.js
+++ src/App.js
@@ -1,19 +1,60 @@
 import { useState } from 'react';
 
 function App() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [idade, setIdade] = useState('');
+
+  const [user, setUser] = useState({});
+
+  function handleRegister(e) {
+    e.preventDefault();
+    //alert('TESTE');
+
+    alert('Usuário registrado com sucesso!')
+    setUser({
+      nome: nome,
+      idade: idade,
+      email: email,
+    })
+  }
   
   return(
     <div>
-      <form>
+      <h1>Cadastrando usuário</h1>
+
+      <form onSubmit={handleRegister}>
         <label>Nome:</label><br />
-        <input placeholder='Digite seu nome' /><br />
+        <input 
+          placeholder='Digite seu nome' 
+          value={nome}
+          onChange={ (e) => setNome(e.target.value) }
+        /><br />
 
         <label>Email:</label><br />
-        <input placeholder='Digite seu email' /><br />
+        <input 
+          placeholder='Digite seu email' 
+          value={email}
+          onChange={ (e) => setEmail(e.target.value) }
+        /><br />
 
         <label>Idade:</label><br />
-        <input placeholder='Digite sua idade' /><br />
+        <input 
+          placeholder='Digite sua idade' 
+          value={idade}
+          onChange={ (e) => setIdade(e.target.value) }
+        /><br />
+
+        <button type="submit">Registrar</button>
       </form>
+
+      <br/><br/>
+
+      <div>
+        <span>Bem vindo: {user.nome}</span><br/>
+        <span>Idade: {user.idade}</span><br/>
+        <span>Email: {user.email}</span><br/>
+      </div>
     </div>
   );
 }
